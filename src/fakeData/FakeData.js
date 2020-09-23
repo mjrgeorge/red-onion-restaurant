b30ecaaf30f0ac33bde390af453e0de4ff2321cc
mjrgeorge <mjrgeorge@gmail.com>
2020-09-23 07:30:26 +0600

    red-onion-restaurent website start

---
+++ src/fakeData/FakeData.js
@@ -0,0 +1,165 @@
+const FakeData = [{
+        "id": "1",
+        "price": 9.90,
+        "category": "breakfast",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/W1gKm1pW/breakfast1.png"
+    },
+    {
+        "id": "2",
+        "price": 7.90,
+        "category": "breakfast",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/vT424QSv/breakfast2.png"
+    },
+    {
+        "id": "3",
+        "price": 12.90,
+        "category": "breakfast",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/8Cb03X6L/breakfast3.png"
+    },
+    {
+        "id": "4",
+        "price": 10.90,
+        "category": "breakfast",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/ydYbzhmt/breakfast4.png"
+    },
+    {
+        "id": "5",
+        "price": 13.90,
+        "category": "breakfast",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/KzqscKQz/breakfast5.png"
+    },
+    {
+        "id": "6",
+        "price": 11.90,
+        "category": "breakfast",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/GpRfXnHc/breakfast6.png"
+    },
+    {
+        "id": "7",
+        "price": 36.90,
+        "category": "lunch",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/tRPpvpg6/lunch1.png"
+    },
+    {
+        "id": "8",
+        "price": 33.90,
+        "category": "lunch",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/DwfhzGPQ/lunch2.png"
+    },
+    {
+        "id": "9",
+        "price": 35.90,
+        "category": "lunch",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/jS8bnjMp/lunch3.png"
+    },
+    {
+        "id": "10",
+        "price": 37.90,
+        "category": "lunch",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/rm4XHNqT/lunch4.png"
+    },
+    {
+        "id": "11",
+        "price": 41.90,
+        "category": "lunch",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/g0Qb4TF4/lunch5.png"
+    },
+    {
+        "id": "12",
+        "price": 55.90,
+        "category": "lunch",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/GpH1F1NH/lunch6.png"
+    },
+    {
+        "id": "13",
+        "price": 25.90,
+        "category": "dinner",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/Y2NTm99d/dinner1.png"
+    },
+    {
+        "id": "14",
+        "price": 22.90,
+        "category": "dinner",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/2SttfLcS/dinner2.png"
+    },
+    {
+        "id": "15",
+        "price": 27.90,
+        "category": "dinner",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/vZTKjtvK/dinner3.png"
+    },
+    {
+        "id": "16",
+        "price": 37.90,
+        "category": "dinner",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/NMqPKqwh/dinner4.png"
+    },
+    {
+        "id": "17",
+        "price": 35.90,
+        "category": "dinner",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/JhB277vc/dinner5.png"
+    },
+    {
+        "id": "18",
+        "price": 19.90,
+        "category": "dinner",
+        "item": "Lorem Ipsum  menu",
+        "details": "Nulla dapibus molestie mi, vitae ultrices ipsum. Cras ultrices tincidunt lectus in sollicitudin. Nullam consequat bibendum commodo. Aliquam eu diam maximus, blandit nisl vitae, cursus libero. Donec fermentum a augue ut lobortis. Donec consequat elit id lorem laoreet, in imperdiet arcu luctus. Integer enim ligula.",
+        "description": "Lorem text of the printing and typesetting industry.",
+        "image": "https://i.postimg.cc/RVX8XDwf/dinner6.png"
+    }
+];
+
+export default FakeData;

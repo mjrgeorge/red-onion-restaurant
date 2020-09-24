1d974518edd47fb1c1dd76b5cee5d83732190db6
mjrgeorge <mjrgeorge@gmail.com>
2020-09-24 08:56:32 +0600

    setup completed for firebase auth

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import SignIn from './signIn/SignIn';
 import SignUp from './signUp/SignUp';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebaseConfig";
+
+firebase.initializeApp(firebaseConfig);
 
 const Login = () => {
+
+
+
     return (
         <div>
             <SignUp/>
---
+++ src/components/login/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCptaqVz9xbJkXgy5uM67bKKooV7LW0jQ",
+    authDomain: "red-onion-restaurant-2611b.firebaseapp.com",
+    databaseURL: "https://red-onion-restaurant-2611b.firebaseio.com",
+    projectId: "red-onion-restaurant-2611b",
+    storageBucket: "red-onion-restaurant-2611b.appspot.com",
+    messagingSenderId: "72331248070",
+    appId: "1:72331248070:web:3832b7ecf34665943772c7"
+  };
+
+  export default firebaseConfig;

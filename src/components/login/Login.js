e0f39796b5f3f6dc0a05e90b8106c328add183fc
mjrgeorge <mjrgeorge@gmail.com>
2020-09-24 13:09:02 +0600

    user created added

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,19 +1,75 @@
-import React from 'react';
-import SignIn from './signIn/SignIn';
-import SignUp from './signUp/SignUp';
+import React, { useState } from 'react';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from "./firebaseConfig";
+import SignUp from './signUp/SignUp';
+import SignIn from './signIn/SignIn';
 
 firebase.initializeApp(firebaseConfig);
 
 const Login = () => {
 
+const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    error: '',
+    success: false,
+});
 
+const handleBlur = (e) => {
+
+    let isFieldValid = true;
+
+    if(e.target.name === 'name'){
+        const nameValid = /^[a-zA-Z]/.test(e.target.value);
+        isFieldValid=nameValid;
+    }
+    if(e.target.name === 'email'){
+        const emailValid = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/.test(e.target.value);
+        isFieldValid=emailValid;
+    }
+    if(e.target.name === 'password'){
+        const passwordValid = /^[#\w@_-]{6,20}$/.test(e.target.value);
+        isFieldValid=passwordValid;
+    }
+    if(isFieldValid){
+        const newUserInfo = {...user};
+        newUserInfo[e.target.name]=e.target.value;
+        setUser(newUserInfo);
+    }
+};
+
+const handleSingUp = (e) => {
+if(user.password===user.confirmPassword){
+    if(user.name && user.email && user.password && user.confirmPassword){
+        firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+        .then(response =>{
+            const newUserInfo = {...user};
+            newUserInfo.success = true;
+            newUserInfo.error = "";
+            setUser(newUserInfo);
+        })
+        .catch(error=>{
+            const errorMessage = error.message;
+            const newUserInfo = {...user};
+            newUserInfo.success = false;
+            newUserInfo.error = errorMessage;
+            setUser(newUserInfo);
+          });
+    }
+}else{
+    alert("Don't Match Password");
+};
+
+e.preventDefault();
+};
 
     return (
         <div>
-            <SignUp/>
+            <SignUp user={user} handleBlur={handleBlur} handleSingUp={handleSingUp}/>
             <SignIn/>
         </div>
     );
--- src/components/login/signUp/SignUp.js
+++ src/components/login/signUp/SignUp.js
@@ -2,29 +2,32 @@ import React from 'react';
 import { Button, Card, Form } from 'react-bootstrap';
 import logo from '../../../images/logo.png';
 
-const SignUp = () => {
+const SignUp = (props) => {
+    const {handleBlur, handleSingUp, user} = props;
     return (
         <div className="d-flex justify-content-center">
-            <Card className="p-4 m-5" style={{width: '400px'}}>
-                <Card.Img className="pb-4" src={logo} alt=""/>
-                <Form>
-                    <Form.Group controlId="formGroupEmail">
-                        <Form.Control type="text" name="name" placeholder="Name" required/>
-                    </Form.Group>
-                    <Form.Group controlId="formGroupEmail">
-                        <Form.Control type="email" name="email" placeholder="Email" required/>
-                    </Form.Group>
-                    <Form.Group controlId="formGroupPassword">
-                        <Form.Control type="password" name="password" placeholder="Password" required/>
-                    </Form.Group>
-                    <Form.Group controlId="formGroupPassword">
-                        <Form.Control type="password" name="confirm_password" placeholder="Confirm Password" required/>
-                    </Form.Group>
-                    <Button variant="danger" block type="submit">Sign Up</Button>
-                    <Button className="text-danger" block variant="link">Already have an account</Button>
-                </Form>
-            </Card>
-        </div>
+        <Card className="p-4 m-5" style={{width: '400px'}}>
+            <Card.Img className="pb-4" src={logo} alt=""/>
+            <Form onSubmit={handleSingUp}>
+                <Form.Group controlId="formGroupEmail">
+                    <Form.Control onBlur={handleBlur} type="text" name="name" placeholder="Name" required/>
+                </Form.Group>
+                <Form.Group controlId="formGroupEmail">
+                    <Form.Control onBlur={handleBlur} type="email" name="email" placeholder="Email" required/>
+                </Form.Group>
+                <Form.Group controlId="formGroupPassword">
+                    <Form.Control onBlur={handleBlur} type="password" name="password" placeholder="Password" required/>
+                </Form.Group>
+                <Form.Group controlId="formGroupPassword">
+                    <Form.Control onBlur={handleBlur} type="password" name="confirmPassword" placeholder="Confirm Password" required/>
+                </Form.Group>
+                <Button variant="danger" block type="submit">Sign Up</Button>
+                {user.success&&<p className="text-success text-center mt-4">User Created Successfully</p>}
+                <p className="text-danger text-center mt-4">{user.error}</p>
+                <Button block variant="link">Already have an account</Button>
+            </Form>
+        </Card>
+    </div>
     );
 };
 

1b17dfb10ac8d16c29487be16b01c79c4428cd15
mjrgeorge <mjrgeorge@gmail.com>
2020-09-24 19:36:22 +0600

    contact component added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Button, Card, Form } from 'react-bootstrap';
+import bike from '../../images/bike.png';
 
 const Contact = () => {
     return (
-        <div>
-            <h1>This is Contact Component</h1>
+        <div className="row">
+            <div className="col-md-6 d-flex justify-content-center">
+                <Card className="p-4 m-5" style={{width: '400px'}}>
+                    
+                </Card>
+            </div>
+            <div className="col-md-6">
+                <div className="d-flex justify-content-center">
+                    <Card className="p-4 m-5" style={{width: '400px'}}>
+                        <Card.Img className="pb-4 w-25" src={bike} alt="Logo"/>
+                        <Card.Header>
+                            <ul>
+                                <li>Your Location</li>
+                                <p><small>107 Rd Restaurant 8</small></p>
+                                <br/>
+                                <li>Shop Address</li>
+                                <p><small>Gulshan Piaza Restaurant</small></p>
+                            </ul>
+                            <Card.Body>
+                                <Card.Title>
+                                    <h2>09:30 PM</h2>
+                                    <p><small>Estimated Delivery Time</small></p>
+                                </Card.Title>
+                                <Card.Text>
+                                    <p><small>Your Rider</small></p>
+                                    <h4>Hamim</h4>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card.Header>
+                        <Button variant="danger" block type="submit">Sign Up</Button>
+                    </Card>
+                </div>
+            </div>
         </div>
     );
 };
